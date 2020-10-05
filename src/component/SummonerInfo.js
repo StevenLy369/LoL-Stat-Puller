f282228660aa556e35b08a7560d6bd95741e8b6c
Steven Ly <1stevenly92@gmail.com>
2020-10-05 09:28:33 -0700

    refactored summonerinfo

--- src/component/SummonerInfo.js
+++ src/component/SummonerInfo.js
@@ -6,18 +6,16 @@ import PropTypes from "prop-types";
 
 
 function SummonerInfo(props){
-
-     
-    
+    const {SummonerDTO, AccountInfo} = props;
      
 
     return (
         <React.Fragment>
           
              
-             <p>Summoner Name: {props.SummonerDTO.name}</p>
-             <p>Summoner Level: {props.SummonerDTO.summonerLevel}</p>
-             <img src= {`http://ddragon.bangingheads.net/cdn/10.19.1/img/profileicon/${props.SummonerDTO.profileIconId}.png`} alt="profile-icon"></img>
+             <p>Summoner Name: {SummonerDTO.name}</p>
+             <p>Summoner Level: {SummonerDTO.summonerLevel}</p>
+             <img src= {`http://ddragon.bangingheads.net/cdn/10.19.1/img/profileicon/${SummonerDTO.profileIconId}.png`} alt="profile-icon"></img>
              
              
                     
