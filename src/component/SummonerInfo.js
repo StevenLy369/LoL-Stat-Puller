d309fdd05429b64a22d85d97b44a845fb40c285a
Steven Ly <1stevenly92@gmail.com>
2020-10-05 12:20:14 -0700

    displaying rank/wins/losses and lp now

--- src/component/SummonerInfo.js
+++ src/component/SummonerInfo.js
@@ -12,7 +12,11 @@ function SummonerInfo(props) {
         <React.Fragment>
             <p>Summoner Name: {SummonerDTO.name}</p>
             <p>Summoner Level: {SummonerDTO.summonerLevel}</p>
-            <p>{AccountInfo[0] ? AccountInfo[0].tier : null}</p>
+            <p>{AccountInfo[0] ? AccountInfo[0].tier : null} {AccountInfo[0] ? AccountInfo[0].rank : null}</p>
+            <p>Wins:{AccountInfo[0] ? AccountInfo[0].wins : null}</p>
+            <p>Losses:{AccountInfo[0] ? AccountInfo[0].losses : null}</p>
+            <p>Lp: {AccountInfo[0] ? AccountInfo[0].leaguePoints : null}</p>
+
            
             <img src={`http://ddragon.bangingheads.net/cdn/10.19.1/img/profileicon/${SummonerDTO.profileIconId}.png`} alt="profile-icon"></img>
         </React.Fragment>
