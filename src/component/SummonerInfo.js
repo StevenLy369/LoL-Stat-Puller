dbeb507535f6899d3a3a7ed22166f28cc0d6164e
Steven Ly <1stevenly92@gmail.com>
2020-10-05 16:29:21 -0700

    got badges to display, now working on displaying solo and flex rank

--- src/component/SummonerInfo.js
+++ src/component/SummonerInfo.js
@@ -17,15 +17,18 @@ function SummonerInfo(props) {
     //     }
 
     // }
-
+   
     return (
         <React.Fragment>
             <p>Summoner Name: {SummonerDTO.name}</p>
             <p>Summoner Level: {SummonerDTO.summonerLevel}</p>
             <p>{AccountInfo[0] ? AccountInfo[0].tier : null} {AccountInfo[0] ? AccountInfo[0].rank : null}</p>
-
             <p>{AccountInfo[0] && AccountInfo[0].tier ==="PLATINUM" ? <img src= {`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/content/src/leagueclient/rankedcrests/05_platinum/images/platinum_baseface_matte.png`} alt="rank metal"/> : null}</p>
-
+            <p>{AccountInfo[0] && AccountInfo[0].tier ==="MASTER" ? <img src= {`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/content/src/leagueclient/rankedcrests/07_master/images/master_baseface_matte.png`} alt="rank metal"/> : null}</p>
+            <p>{AccountInfo[0] && AccountInfo[0].tier ==="SILVER" ? <img src= {`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/content/src/leagueclient/rankedcrests/03_silver/images/silver_baseface_matte.png`} alt="rank metal"/> : null}</p>
+            <p>{AccountInfo[0] && AccountInfo[0].tier ==="GOLD" ? <img src= {`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/content/src/leagueclient/rankedcrests/04_gold/images/gold_baseface_matte.png`} alt="rank metal"/> : null}</p>
+            <p>{AccountInfo[0] && AccountInfo[0].tier ==="DIAMOND" ? <img src= {`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/content/src/leagueclient/rankedcrests/06_diamond/images/diamond_baseface_matte.png`} alt="rank metal"/> : null}</p>
+            <p>{AccountInfo[0] && AccountInfo[0].tier ==="GRANDMASTER" ? <img src= {`https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/content/src/leagueclient/rankedcrests/08_grandmaster/images/grandmaster_baseface_matte.png`} alt="rank metal"/> : null}</p>
             <p>Wins:{AccountInfo[0] ? AccountInfo[0].wins : null}</p>
             <p>Losses:{AccountInfo[0] ? AccountInfo[0].losses : null}</p>
             <p>Lp: {AccountInfo[0] ? AccountInfo[0].leaguePoints : null}</p>
